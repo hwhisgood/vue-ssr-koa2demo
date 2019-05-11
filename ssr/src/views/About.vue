fcc137126f0f33a99a8556be81772fbd0cc18022
hwhisgood <234516742@qq.com>
2019-05-11 18:24:17 +0800

    upload base code

---
+++ ssr/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <welcome :name="about"></welcome>
+  </div>
+</template>
+<script>
+import Welcome from "@/components/Welcome";
+export default {
+  name: "ViewAbout",
+  components: {
+    Welcome
+  },
+  data() {
+    return {
+      about:'测试'
+    };
+  }
+};
+</script>
+<style module>
+.about {
+  color: red;
+}
+</style>
